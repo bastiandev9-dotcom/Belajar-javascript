21817458094cbae276e339894fa710b6d6ec7ba9
Ogi Bastian <bastiandev9@gmail.com>
2026-01-02 23:25:17 +0700

    add basic javascript

---
+++ variabel-tipeData.js
@@ -0,0 +1,42 @@
+//variabel : var, let, const
+/*var cek = "tes";
+let umur = 18;
+const nama = "Ogi sebastian";
+
+console.log(cek, nama, umur);*/
+
+//tipe data 
+/*let kata = "Hello World"; //string
+let number = 18; // angka
+let pi = 3.14; //angka
+let benar = true; // boolean
+let nilai // undefined(belum diberi nilai)
+let kosong = null; // kosong(disengaja)
+
+console.log(kata, number, pi, benar, nilai, kosong);*/
+
+//latihan
+let nama = "Ogi";
+const tahun = 2007;
+console.log(nama, tahun);
+console.log("--------------------------------------------------");
+
+let hobi = "ngoding";
+hobi = "makan";
+console.log(hobi);
+console.log("-------------------------------------------------");
+
+let kata1 = "Backend";
+let kata2 =  "Developer";
+console.log(kata1, kata2);
+console.log("--------------------------------------------------");
+
+let a = 25;
+let b = 10;
+console.log(`penjumlahan ${a + b}, pengurangan ${a-b}, perkalian ${a*b}`);
+console.log("-----------------------------------------------");
+
+let halo = "bastian";
+let umur = 18;
+let suka_ngoding = true;
+console.log(`${halo} berumur ${umur} dan suka ngoding: ${suka_ngoding} `);
