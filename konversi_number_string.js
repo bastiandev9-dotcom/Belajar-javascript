19bd588d1dc7ff4c5ec6790eab7ec7e0c297e05d
Ogi Bastian <bastiandev9@gmail.com>
2026-01-07 12:30:29 +0700

    add basic javascript

---
+++ konversi_number_string.js
@@ -0,0 +1,14 @@
+//parseInt
+let harga = "42000.90";
+let new_harga = parseInt(harga);
+console.log(`harga string: "${harga}", harga number: ${new_harga}`)
+
+//parseFloat
+let a = "42000.90";
+let b = parseFloat(harga);
+console.log(`harga string: "${a}", harga number: ${b}`)
+
+//toString
+let c = 450;
+let d = c.toString()
+console.log(`number ${c} diubah ke string: ${d}`)
